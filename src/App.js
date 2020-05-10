a4b47337b9ebc3cafec2902d0b613865effeace8
Alan Russell <ajrussellaudio@gmail.com>
2020-05-10 23:54:45 +0100

    Style tempo input

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { useState } from 'react';
 import TempoInput from './components/TempoInput';
 import BeatsPerSecond from './components/BeatsPerSecond';
 import PeriodOfBeat from './components/PeriodOfBeat';
+import './App.sass';
 
 function App() {
     const [tempo, setTempo] = useState(120);
 
     return (
-        <React.Fragment>
+        <div className="container">
             <TempoInput tempo={tempo} setTempo={setTempo} />
             <BeatsPerSecond tempo={tempo} />
             <PeriodOfBeat tempo={tempo} />
-        </React.Fragment>
+        </div>
     );
 }
 
--- src/components/TempoInput.jsx
+++ src/components/TempoInput.jsx
@@ -2,19 +2,55 @@ import React from 'react';
 
 const TempoInput = ({ tempo, setTempo }) => {
     const handleChange = (event) => {
-        setTempo(event.target.value);
+        setTempo(parseFloat(event.target.value));
     };
+
+    const handleButtonClick = (value) => () => setTempo(tempo + value);
+
     return (
-        <div>
-            <label htmlFor="tempo">Enter Tempo (BPM)</label>
-            <input
-                type="number"
-                id="tempo"
-                value={tempo}
-                step={0.1}
-                onChange={handleChange}
-            />
-        </div>
+        <section class="section">
+            <label className="field-label" htmlFor="tempo">
+                Enter Tempo (BPM)
+            </label>
+            <div className="field has-addons">
+                <div className="control">
+                    <button
+                        class="button is-primary"
+                        onClick={handleButtonClick(-1)}
+                    >
+                        - 1
+                    </button>
+                </div>
+                <div className="control">
+                    <button class="button" onClick={handleButtonClick(-0.1)}>
+                        - 0.1
+                    </button>
+                </div>
+                <div class="control">
+                    <input
+                        className="input"
+                        type="number"
+                        id="tempo"
+                        value={tempo.toPrecision(4)}
+                        step={0.1}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="control">
+                    <button class="button" onClick={handleButtonClick(0.1)}>
+                        + 0.1
+                    </button>
+                </div>
+                <div className="control">
+                    <button
+                        class="button is-primary"
+                        onClick={handleButtonClick(1)}
+                    >
+                        + 1
+                    </button>
+                </div>
+            </div>
+        </section>
     );
 };
 
