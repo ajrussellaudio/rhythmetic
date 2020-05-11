f50660439a6b2a7204d8619d899587c850a0d17d
Alan Russell <ajrussellaudio@gmail.com>
2020-05-11 21:19:10 +0100

    Add GitHub link

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import PeriodOfBeat from './components/PeriodOfBeat';
 import LengthInSamples from './components/LengthInSamples';
 import DelayTime from './components/DelayTime';
 import './App.sass';
+import GitHubLink from './components/GitHubLink';
 
 function App() {
     const [tempo, setTempo] = useState(120);
 
     return (
-        <section className="section">
-            <div className="container">
-                <TempoInput tempo={tempo} setTempo={setTempo} />
-                <BeatsPerSecond tempo={tempo} />
-                <PeriodOfBeat tempo={tempo} />
-                <DelayTime tempo={tempo} />
-                <LengthInSamples tempo={tempo} />
-            </div>
-        </section>
+        <React.Fragment>
+            <GitHubLink />
+            <section className="section">
+                <div className="container">
+                    <TempoInput tempo={tempo} setTempo={setTempo} />
+                    <BeatsPerSecond tempo={tempo} />
+                    <PeriodOfBeat tempo={tempo} />
+                    <DelayTime tempo={tempo} />
+                    <LengthInSamples tempo={tempo} />
+                </div>
+            </section>
+        </React.Fragment>
     );
 }
 
---
+++ src/components/GitHubLink.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './GitHubLink.scss';
+
+const GitHubLink = () => (
+    <div className="github-link">
+        <a class="icon" href="https://github.com/ajrussellaudio/rhythmetic">
+            <i class="fab fa-github fa-lg"></i>
+        </a>
+    </div>
+);
+
+export default GitHubLink;
