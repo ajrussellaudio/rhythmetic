62e42964368d8e13c2cd11fe23ff9f9a82730fe2
Alan Russell <ajrussellaudio@gmail.com>
2020-05-11 21:36:57 +0100

    Updates

---
+++ precache-manifest.9ce46dc976c26fda9e9b02f236d1a369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983fb5a48d2f681eb18162408aa3ccc6",
+    "url": "/rhythmetic/index.html"
+  },
+  {
+    "revision": "e16b80cac483f3b817e3",
+    "url": "/rhythmetic/static/css/main.30c2219c.chunk.css"
+  },
+  {
+    "revision": "b791d8479ab2ef7ecb1f",
+    "url": "/rhythmetic/static/js/2.9d40f916.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rhythmetic/static/js/2.9d40f916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e16b80cac483f3b817e3",
+    "url": "/rhythmetic/static/js/main.2569291c.chunk.js"
+  },
+  {
+    "revision": "e6538151979efc696462",
+    "url": "/rhythmetic/static/js/runtime-main.f8efb6d5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/rhythmetic/precache-manifest.9ce46dc976c26fda9e9b02f236d1a369.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rhythmetic/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
