f3b9481bba0b208448c26d3236ad97dfc81fe619
Alan Russell <ajrussellaudio@gmail.com>
2020-05-17 14:38:13 +0100

    Use box output everywhere

--- src/components/BeatsPerSecond.jsx
+++ src/components/BeatsPerSecond.jsx
@@ -3,14 +3,34 @@ import beatsPerSecond from '../utils/beatsPerSecond';
 
 const BeatsPerSecond = ({ tempo }) => (
     <div className="box">
-        is equal to{' '}
-        <strong>
-            {beatsPerSecond(tempo).toLocaleString(undefined, {
-                minimumFractionDigits: 1,
-                maximumFractionDigits: 5,
-            })}
-        </strong>{' '}
-        beats per second
+        <div className="field is-horizontal">
+            <div className="field-label is-normal">
+                <label className="label">is equal to</label>
+            </div>
+            <div className="field-body">
+                <div className="field has-addons">
+                    <div className="control">
+                        <input
+                            className="input"
+                            type="text"
+                            readOnly
+                            value={beatsPerSecond(tempo).toLocaleString(
+                                undefined,
+                                {
+                                    minimumFractionDigits: 1,
+                                    maximumFractionDigits: 5,
+                                }
+                            )}
+                        />
+                    </div>
+                    <div className="control">
+                        <button className="button is-static">
+                            Hz (beats per second)
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 );
 
--- src/components/PeriodOfBeat.jsx
+++ src/components/PeriodOfBeat.jsx
@@ -3,13 +3,33 @@ import periodOfBeat from '../utils/periodOfBeat';
 
 const PeriodOfBeat = ({ tempo }) => (
     <div className="box">
-        each beat lasts{' '}
-        <strong>
-            {(1000 * periodOfBeat(tempo)).toLocaleString(undefined, {
-                maximumFractionDigits: 1,
-            })}
-        </strong>{' '}
-        milliseconds
+        <div className="field is-horizontal">
+            <div className="field-label is-normal">
+                <label className="label">each beat lasts</label>
+            </div>
+            <div className="field-body">
+                <div className="field has-addons">
+                    <div className="control">
+                        <input
+                            className="input"
+                            type="text"
+                            readOnly
+                            value={(1000 * periodOfBeat(tempo)).toLocaleString(
+                                undefined,
+                                {
+                                    maximumFractionDigits: 1,
+                                }
+                            )}
+                        />
+                    </div>
+                    <div className="control">
+                        <button className="button is-static">
+                            milliseconds
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 );
 
